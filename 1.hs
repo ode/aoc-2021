e79929111fe63495f1d13a17b6ded638a810b666
Harshith Vasireddy <LordPos@github.com>
2021-12-01 12:44:04 +0530

    d1

--- 1.hs
+++ 1.hs
@@ -5,4 +5,5 @@ zipped l = zip l (tail l)
 prepare :: String -> [Int]
 prepare = map read . lines
 
-main = readFile "1.txt" >>=  print . length .filter (\(a,b) -> a < b). zipped . zipped3 . prepare
+main = readFile "1.txt" >>=  print . length . filter (\(a,b) -> a < b) . zipped . zipped3 . prepare 
+-- remove '. zipped3' for p1
